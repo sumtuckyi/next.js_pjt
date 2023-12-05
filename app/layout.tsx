c3b18d33518d0431059e80414ce5401e3390c903
sumtuckyi <sumin510@naver.com>
2023-12-06 03:57:20 +0900

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { ar_one_sans, inter, lusitana } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${ar_one_sans.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { AR_One_Sans } from 'next/font/google'; 
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ["400", "700"]})
+export const ar_one_sans = AR_One_Sans({ subsets: ['latin'], weight: ["400", "700"]})
