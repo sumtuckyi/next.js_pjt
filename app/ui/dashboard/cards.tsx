49ccd90459c2c49ee2a9f258025d4c24463b59cb
sumtuckyi <sumin510@naver.com>
2023-12-13 02:02:12 +0900

    12일

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,7 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { fetchCardData } from '@/app/lib/data';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana, tektur } from '@/app/ui/fonts';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -55,7 +55,7 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
+        className={`${tektur.className}
           truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,9 @@
 import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
-import { AR_One_Sans } from 'next/font/google'; 
+import { AR_One_Sans } from 'next/font/google';
+import { Tektur } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ subsets: ['latin'], weight: ["400", "700"]})
-export const ar_one_sans = AR_One_Sans({ subsets: ['latin'], weight: ["400", "700"]})
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ["400", "700"]});
+export const ar_one_sans = AR_One_Sans({ subsets: ['latin'], weight: ["400", "700"]});
+export const tektur = Tektur({ subsets: ['latin'], weight: ["400", "700"] });
